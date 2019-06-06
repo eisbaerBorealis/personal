a787ea6c0d6151f8fc3538b6a1cfe68f908cf957
Jesse Howell <eisbaer.borealis@gmail.com>
2019-06-05 23:53:12 -0600

    Starting Python exercises
    Got Hello World out of the way, started the exercise list at practicepython.org

---
+++ learning/python/exercises/00-hello-world.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello world")
+
+main()
+
+# after adding python to the PATH, navigate to this folder
+#  and enter "py 00-hello-world.py"
---
+++ learning/python/exercises/pp01-input.py
@@ -0,0 +1,10 @@
+def main():
+    input_name = input("What is your name? ")
+    input_birth_year = int(input("In which year were you born? "))
+    
+    print('Thank you, {}. You will turn 100 in {}.'.format(input_name, input_birth_year + 100)) 
+
+main()
+
+# Exercises found here:
+#  https://www.practicepython.org/

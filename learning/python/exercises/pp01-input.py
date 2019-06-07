3604e78fcbf79911aaa6bab5cd224603dffb0795
Jesse Howell <eisbaer.borealis@gmail.com>
2019-06-06 23:40:51 -0600

    Improve pp01
    Added input validation to get a reasonable integer for the birth year.

--- learning/python/exercises/pp01-input.py
+++ learning/python/exercises/pp01-input.py
@@ -1,8 +1,19 @@
 def main():
-    input_name = input("What is your name? ")
-    input_birth_year = int(input("In which year were you born? "))
-    
-    print('Thank you, {}. You will turn 100 in {}.'.format(input_name, input_birth_year + 100)) 
+	input_name = input("What is your name? ")
+	
+	good_input = False
+	
+	while(not good_input):
+		try:
+			input_birth_year = int(input("In which year were you born? "))
+			if(input_birth_year > 1900 and input_birth_year < 2019):
+				good_input = True
+			else:
+				print('Error: birth year must be between 1900 and 2019.')
+		except:
+			print('Error: must enter an integer for your birth year.')
+	
+	print('Thank you, {}. You will turn 100 in {}.'.format(input_name, input_birth_year + 100)) 
 
 main()
 

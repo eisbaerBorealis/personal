ec1f045dea7589ac09bce985e56805f8db475448
Jesse Howell <eisbaer.borealis@gmail.com>
2019-08-15 14:11:53 -0600

    Finished Lesson 5 of the VueMastery intro tutorial

--- learning/vue/main.js
+++ learning/vue/main.js
@@ -1,6 +1,31 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: './assets/vmSocks-green.jpg',
+        altText: 'A pair of green socks',
+        inventory: 7.5,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './assets/vmSocks-blue.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1;
+        },
+        updateProduct: function(variantImage) {
+            this.image = variantImage;
+        }
     }
 })

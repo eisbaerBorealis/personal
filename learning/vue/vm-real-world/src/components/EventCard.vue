c99a1ea13c35fdad0aca086ced17c9cf38b7a76c
Jesse Howell <eisbaer.borealis@gmail.com>
2019-08-27 13:58:27 -0600

    Split Vue tutorials, initialize Real World Vue.js tutorial
    From VueMastery

---
+++ learning/vue/vm-real-world/src/components/EventCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }">
+        <div class="event-card -shadow">
+            <span class="eyebrow">At {{ event.time }} on {{  event.date  }}</span>
+            <h4 class="title">{{ event.title }}</h4>
+            <BaseIcon name="users"/>
+            <span>{{ event.attendees.length }} attending!</span>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            event: {
+                id: 1,
+                title: 'Park Cleanup',
+                date: 'Tues August 19, 2018',
+                time: '5:30 pm',
+                attendees: [
+                    {id: 'abc123', name: 'Jesse Howell'},
+                    {id: 'def456', name: 'Colby Howell'}
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.event-card {
+    padding: 20px;
+    margin-bottom: 24px;
+    transition: all 0.2s linear;
+    cursor: pointer;
+}
+.event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+    margin: 0;
+}
+
+.event-link {
+    color: black;
+    text-decoration: none;
+    font-weight: 100;
+}
+</style>
---
+++ learning/vue/vm-real-world/src/views/EventShow.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Showing Event #{{ id }}</h1>
+</template>
+
+<script>
+export default {
+  props: ["id"]
+};
+</script>

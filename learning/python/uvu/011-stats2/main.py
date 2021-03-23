f99fb41a42058c4a909bb6a157fdc5841eb0ca98
Jesse Howell <eisbaer.borealis@gmail.com>
2021-03-22 18:34:59 -0600

    Cleanup - now displays and animates simply histograms

--- learning/python/uvu/011-stats2/main.py
+++ learning/python/uvu/011-stats2/main.py
@@ -1,16 +1,11 @@
 from myStats import *
-# from Graph import *
 
 def main():
     expon1 = getExponData(10000)
     expon2 = getExponData(10, 4)
 
-    # testExponential()
-
-    print('expon1 is', expon1)
-    print('expon2 is', expon2)
-
-    displayExponData(expon1)
+    displayHistogram(expon1)
+    animateHistogram(expon1)
 
 main()
 

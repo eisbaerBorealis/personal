e3c7555d6191879dfc05eb0039fd986cdf948aa0
Jesse Howell <eisbaer.borealis@gmail.com>
2021-12-21 09:44:53 -0700

    Old files

---
+++ learning/python/uvu/013-projE/project_e.py
@@ -0,0 +1,46 @@
+'''
+    p5.py: Illustrates the payroll module.
+'''
+
+from payroll import *
+import shutil
+
+def main():
+    load_employees()
+    process_timecards()
+    process_receipts()
+    run_payroll()
+
+    # Save copy of payroll file; delete old file
+    shutil.copyfile(PAY_LOGFILE, 'paylog_old.txt')
+    if os.path.exists(PAY_LOGFILE):
+        os.remove(PAY_LOGFILE)
+
+    # Change Karina Gay to Salaried and MailMethod by changing her Employee object:
+    emp = find_employee_by_id('688997')
+    emp.make_salaried(45884.99)
+    emp.mail_method()
+    emp.issue_payment()
+
+    # Change TaShya Snow to Commissioned and DirectMethod; add some receipts
+    emp = find_employee_by_id('522759')
+    emp.make_commissioned(50005.50, 25)
+    emp.direct_method('30417353-K', '465794-3611')
+    clas = emp.classification
+    clas.add_receipt(1109.73)
+    clas.add_receipt(746.10)
+    emp.issue_payment()
+
+    # Change Rooney Alvarado to Hourly; add some hour entries
+    emp = find_employee_by_id('165966')
+    emp.make_hourly(21.53)
+    clas = emp.classification
+    clas.add_timecard(8.0)
+    clas.add_timecard(8.0)
+    clas.add_timecard(8.0)
+    clas.add_timecard(8.0)
+    clas.add_timecard(8.0)
+    emp.issue_payment()
+
+if __name__ == '__main__':
+    main()

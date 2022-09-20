a2667d69d0a1be2128a4e434c182c14aaadf29fd
Jesse Howell <eisbaer.borealis@gmail.com>
2022-09-20 07:20:45 -0600

    Finished fishingTest() and normalFishing()
    normalFishing does not work quite as expected. Clicks too fast after a few fish, almost as though the click were being stored and immediately applied as the next one is hooked.

--- game-bots/farming/farmHelper.js
+++ game-bots/farming/farmHelper.js
@@ -23,36 +23,41 @@ function mealwormFishing(newCount) {
 
 function normalFishing(newCount) {
     let count = newCount;
+    let fishOn = false;
+    let fishCountDown = 0;
     console.log('DEBUG: starting normalFishing with count ' + count);
+    
     let fishingInterval = setInterval(() => {
-        let fishes = document.getElementsByClassName('fish');
-        for(let i = 0; i < 12; i++) {
-            let fish = fishes[i];
-            if(fish.style.display !== 'none' && fish.style.opacity > 0.3) {
-                if(Math.random() > 0.87) { // 0.85?
+        if(fishOn) {
+            fishCountDown--;
+            if(fishCountDown <= 0) {
+                console.log('    DEBUG: fishCountDown is ' + fishCountDown);
+                if(Math.random() > 0.5) {
                     count--;
-                    console.log('  DEBUG: Caught a fish! Remaining count: ' + count);
-                    fish.click();
+                    fishOn = false;
+                    console.log('    DEBUG: Caught a fish!');
+                    document.getElementsByClassName('fishcaught')[0].click();
+                }
+            } else {console.log('    DEBUG: fishCountDown is ' + fishCountDown);}
+        } else {
+            let fishes = document.getElementsByClassName('fish');
+            for(let i = 0; i < fishes.length; i++) {
+                let fish = fishes[i];
+                if(fish.style.display !== 'none' && fish.style.opacity > 0.3) {
+                    if(Math.random() > 0.8) {
+                        fishOn = true;
+                        console.log('  DEBUG: Hooked a fish!');
+                        fish.click();
+                        fishCountDown = 10 + Math.floor(Math.random() * 5);
+                        console.log('    DEBUG: new fishCountDown is ' + fishCountDown);
+                    }
                 }
             }
         }
 
         if(count <= 0) {
-            console.log('    DEBUG: caught enough fish!');
+            console.log('DEBUG: caught enough fish!');
             clearInterval(fishingInterval);
-        }
-    }, 100);
-}
-
-function fishingTest() {
-    let fishClicked = false;
-    let fishOn = false;
-
-    let testInterval = setInterval(() => {
-        let fishes = document.getElementsByClassName('fish');
-        if(fishes[0].style.display !== 'none')
-        {
-            console.log('  DEBUG: fishes[0] opacity is ' + fishes[0].style.opacity); // this prints 11 times per fish; 7 times >0.3
         }
     }, 100);
 }
@@ -74,5 +79,43 @@ function mealwormTest2() {
         let testString = '' + (randomNum > 0.8);
         console.log('  DEBUG: randomNum is ' + randomNum + ', activate: ' + testString);
     }, 500);
+}
+
+function fishingTest() {
+    let fishOn = false;
+    let fishCaught = false;
+    let fishCountDown = -1;
+
+    let testInterval = setInterval(() => {
+        if(fishOn) {
+            fishCountDown--;
+            if(fishCountDown <= 0) {
+                if(Math.random() > 0.5) {
+                    fishCaught = true;
+                    fishOn = false;
+                    console.log('  DEBUG: Caught a fish!');
+                    document.getElementsByClassName('fishcaught')[0].click()
+                }
+            }
+        } else {
+            let fishes = document.getElementsByClassName('fish');
+            for(let i = 0; i < fishes.length; i++) {
+                let fish = fishes[i];
+                if(fish.style.display !== 'none' && fish.style.opacity > 0.3) {
+                    if(Math.random() > 0.8) {
+                        fishOn = true;
+                        console.log('  DEBUG: Hooked a fish!');
+                        fish.click();
+                        fishCountDown = 6 + Math.floor(Math.random() * 5);
+                    }
+                }
+            }
+        }
+        
+        if(fishCaught) {
+            console.log('    DEBUG: caught enough fish!');
+            clearInterval(testInterval);
+        }
+    }, 100);
 }
 // */

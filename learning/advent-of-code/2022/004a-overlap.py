8206a735cac9e3497c97de41cf7db0242369cc82
Jesse Howell <eisbaer.borealis@gmail.com>
2022-12-03 22:22:11 -0700

    Advent of Code, 2022, Day 04 complete

---
+++ learning/advent-of-code/2022/004a-overlap.py
@@ -0,0 +1,27 @@
+import pathlib
+import re
+
+def main():
+    filePath = pathlib.Path(__file__).parent.absolute()
+    fileName = "input-004b.txt"
+
+    try:
+        fp = open(str(filePath) + "/" + fileName, 'r')
+        overlapCount = 0
+
+        for line in fp:
+            line = line.strip()
+            # print("Checking", line)
+
+            nums = [int(i) for i in re.split('-|,', line)]
+            # print('  nums is', nums)
+
+            if((nums[0] >= nums[2] and nums[1] <= nums[3]) or
+               (nums[0] <= nums[2] and nums[1] >= nums[3])):
+                overlapCount += 1
+
+        print('Overlap count:', overlapCount)
+    except FileNotFoundError:
+        print("\nError: File not found. Please make sure " + fileName + " is in this program's folder and restart the program.")
+
+main()

7586217443a5f8027d7549f9b4903ed9e7c70ce3
Jesse Howell <eisbaer.borealis@gmail.com>
2022-12-05 11:29:33 -0700

    Advent of Code, 2022, Day 05 complete

---
+++ learning/advent-of-code/2022/005a-cargo.py
@@ -0,0 +1,69 @@
+import pathlib
+import re
+
+def main():
+    filePath = pathlib.Path(__file__).parent.absolute()
+    fileName = "input-005b.txt"
+
+    try:
+        fp = open(str(filePath) + "/" + fileName, 'r')
+
+        cargoInput = []
+        cargo = []
+        stackCount = 0
+
+        gettingSteps = False
+        for line in fp:
+            if line[-1] == "\n":
+                line = line[:-1]
+            
+            # print("checking line \"", line, "\"")
+            if gettingSteps:
+                line = line.split(" ")
+                moveCount = int(line[1])
+                moveFrom = int(line[3]) - 1
+                moveTo = int(line[5]) - 1
+                # print("  gettingSteps:", line)
+                # print("  next step: move", moveCount, "from", moveFrom, "to", moveTo)
+                for i in range(moveCount):
+                    cargo[moveTo].append(cargo[moveFrom].pop())
+            else:
+                if stackCount == 0:
+                    # print("    len(line) is", len(line))
+                    stackCount = len(line) // 4 + 1
+                    for i in range(stackCount):
+                        cargo.append([])
+                    print("    stackCount is", stackCount)
+                if line == "":
+                    gettingSteps = True
+                    # print("  empty line found, gettingSteps set to True")
+                    cargoInput = cargoInput[:-1]
+                    cargoInput.reverse()
+                    for cargoLine in cargoInput:
+                        # print("   \"", cargoLine, "\"")
+                        for i in range(stackCount):
+                            cargoChar = cargoLine[i * 4 + 1]
+                            if cargoChar != " ":
+                                # print("found cargoChar", cargoChar, "at i", i)
+                                cargo[i].append(cargoChar)
+                    print()
+                    for cargoStack in cargo:
+                        print("cargoStack:", cargoStack)
+                else:
+                    cargoInput.append(line)
+
+
+        print()
+        for cargoStack in cargo:
+            print("cargoStack:", cargoStack)
+
+        # print('Overlap count:', overlapCount)
+        output = []
+        for cargoStack in cargo:
+            if len(cargoStack) != 0:
+                output.append(cargoStack[-1])
+        print("".join(output))
+    except FileNotFoundError:
+        print("\nError: File not found. Please make sure " + fileName + " is in this program's folder and restart the program.")
+
+main()
